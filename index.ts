308ab7f7fb8617488b53126369e327fc36f6fbb8
Waren Gonzaga <opensource@warengonzaga.com>
2021-11-29 21:03:46 +0800

    ☕ CHORE: initial bot setup

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
-import DiscordJS, { Intents } from 'discord.js';
+import DiscordJS, { Channel, Intents, MessageEmbed } from 'discord.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new DiscordJS.Client({
+const discordToken = process.env.DISCORD_TOKEN;
+const discordLogs = process.env.DISCORD_LOGS_CHANNEL_ID;
+
+const bot = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
     ]
 });
 
-client.on('messageCreate', (message) => {
+bot.on('messageCreate', (message) => {
     if (message.content === 'hello') {
         message.reply({
             content: 'world',
@@ -18,8 +21,29 @@ client.on('messageCreate', (message) => {
     }
 });
 
-client.on('ready', () => {
-    console.log('Ready!');
+// online status
+bot.on('ready', async () => {
+    
+    // bot activity
+    bot.user?.setActivity('Tech Communities', { type: 'WATCHING' });
+
+    // bot status
+    if (discordLogs) {
+        const channel = bot.channels.cache.get(discordLogs);
+        const embed = new DiscordJS.MessageEmbed()
+            .setTitle('Waren is now online!')
+            .setColor('#00ff00');
+        (channel?.isText()) && channel.send({ embeds: [embed] });
+    }
+
+    // bot logs
+    console.log('# Waren is now online!');
 });
 
-client.login(process.env.DISCORD_TOKEN);
+// error handling
+bot.on('error', (error) => {
+    console.log(error);
+});
+
+// authentication
+bot.login(discordToken);
